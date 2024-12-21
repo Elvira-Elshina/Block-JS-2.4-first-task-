488db87d6ea458da521bb83b531f4137027238e2
Elvira Elshina <elvircikfrolenochka@gmail.com>
2024-12-21 10:05:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const input = document.querySelector('.input-form');
+const repositoriesList = document.querySelector(".repositories-list");
+const oneList = document.querySelector(".one-repository-list");
+const ul = document.querySelector('ul');
+
+
+let rpsRepos = [];
+let oneRepos = [];
+
+oneList.addEventListener('click', (e) => {
+  let target = e.target;
+  if (!target.classList.contains("img")) {
+    return;
+  }
+
+  target.parentElement.parentElement.remove();
+});
+
+function repoListTemplate(repo) {
+  oneRepos.push(repo.id);
+  const list = document.createElement("li");
+  
+  list.insertAdjacentHTML('beforeend', `
+        <div class="repoList">
+          Name: ${repo.name}
+          <br>Owner: ${repo.owner.login}
+          <br>Stars: ${repo.stargazers_count}
+          <button>
+          <img src="pngwing.com.png" alt="img" class="img">
+          </button>
+          </div>`
+  );
+
+  oneList.append(list);
+  input.value = "";
+  renderList([]);
+}
+
+repositoriesList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    const repo = rpsRepos.find((el) => el.name === e.target.innerText);
+    if (!oneRepos.includes(repo.id)) {
+      repoListTemplate(repo);
+    }
+    clearRender();
+  }
+});
+
+function clearRender() {
+ repositoriesList.textContent = ''; 
+}
+
+function listTemplate({name}) {
+  const list = `
+    ${name}
+  `;
+  return list;
+}
+
+function renderList(res) {
+  let fragment;
+
+  if (ul.hasChildNodes()) {
+    clearRender();
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (res[i]) {
+      fragment = document.createElement('li');
+      fragment.textContent = listTemplate(res[i]);
+      rpsRepos.push(res[i]);
+      repositoriesList.append(fragment);
+    }
+  }
+}
+
+const debounce = (fn, debounceTime) => {
+  let timeoutId;
+
+  return function (value) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.call(this, value);
+    }, debounceTime);
+  };
+};
+
+function fn(value) {
+  getUsers(value).then((data) => {
+    renderList(data); 
+  });
+}
+
+input.addEventListener("input", (e) => {
+  const value = e.target.value;
+  repos = [];
+  debouncedFn(value);
+});
+
+
+const debouncedFn = debounce(fn, 500);
+async function getUsers(name) {
+  let repos = fetch(`https://api.github.com/users/${name}/repos?per_page=5`).then(
+    (successResponse) => {
+
+      if (successResponse.status != 200) {
+        return null;
+      } else {
+        return successResponse.json();
+      }
+    },
+    (failResponse) => {
+      return null;
+    }
+  );
+
+  return repos;
+}
